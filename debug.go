d969f952af9e02feea59963671298fc236ca4399
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2021-05-07 16:27:37 +0000

    feat: initial implementation
    Refs https://github.com/talos-systems/talos/issues/3534
    
    Signed-off-by: Alexey Palazhchenko <alexey.palazhchenko@gmail.com>

---
+++ debug.go
@@ -0,0 +1,86 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+// debug is a Sidero-specific library for including debugging facilities for developers in our products
+// when they are compiled with sidero.debug build tag. They are not included by default.
+package debug
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"reflect"
+	"runtime"
+	"sort"
+)
+
+// LogFunc is a simplest logging function.
+type LogFunc func(msg string)
+
+// ListenAndServe runs debug server on given address.
+//
+// If debugging is enabled (debug.Enabled is true), the function blocks until fatal error in encountered
+// (it is returned) or ctx is canceled (in that case, nil is returned).
+//
+// If debugging is disabled (debug.Enabled is false), the function immediately returns nil.
+//
+// Both cases can be handled with the following code:
+//
+//   if err := debug.ListenAndServe(ctx, addr, log); err != nil {
+//       log.Fatal(err)
+//   }
+func ListenAndServe(ctx context.Context, addr string, log LogFunc) error {
+	if !Enabled {
+		return nil
+	}
+
+	// that's defaults, just make them explicit
+	runtime.SetCPUProfileRate(100)
+	runtime.MemProfileRate = 512 * 1024
+
+	// https://github.com/DataDog/go-profiler-notes/blob/main/block.md#overhead
+	runtime.SetBlockProfileRate(10000)
+
+	// no science behind that value
+	runtime.SetMutexProfileFraction(10000)
+
+	log("starting debug server")
+
+	for _, h := range handlers() {
+		log(fmt.Sprintf("http://%s%s", addr, h))
+	}
+
+	s := &http.Server{
+		Addr: addr,
+	}
+
+	// no real reason to do a graceful shutdown with s.Shutdown
+	go func() {
+		<-ctx.Done()
+
+		_ = s.Close()
+	}()
+
+	err := s.ListenAndServe()
+	if err == http.ErrServerClosed {
+		return nil
+	}
+
+	return err
+}
+
+// handlers returns all patterns registered on the default HTTP server mux.
+// Useful to check for unexpected handlers.
+func handlers() []string {
+	keys := reflect.ValueOf(http.DefaultServeMux).Elem().FieldByName("m").MapKeys()
+
+	res := make([]string, len(keys))
+	for i, v := range keys {
+		res[i] = v.String()
+	}
+
+	sort.Strings(res)
+
+	return res
+}
---
+++ debug_on_test.go
@@ -0,0 +1,27 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+// +build sidero.debug
+
+package debug
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDebugOn(t *testing.T) {
+	expected := []string{
+		"/debug/events",
+		"/debug/pprof/",
+		"/debug/pprof/cmdline",
+		"/debug/pprof/profile",
+		"/debug/pprof/symbol",
+		"/debug/pprof/trace",
+		"/debug/requests",
+		"/debug/vars",
+	}
+	assert.Equal(t, expected, handlers())
+}
